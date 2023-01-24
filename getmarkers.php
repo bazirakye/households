b4b3c3a57cb2c2d42bcaa8945ed3cf9116fc2504
bazirakye tonny <bazirakyetonny15@gmail.com>
2023-01-24 10:22:00 +0300

    edited fetch markers query

--- getmarkers.php
+++ getmarkers.php
@@ -9,8 +9,17 @@
   Please note that you can use this query if created_at column is unique for each groupId.
   You can also use the order by created_at desc and limit 1 for each groupId.*/
 
-  $sql = "SELECT * FROM markers WHERE WEEK(created_at, 3) = WEEK(CURDATE(), 3) AND YEAR(created_at) = YEAR(CURDATE()) group by groupId order by created_at desc limit 1";
-  $result = mysqli_query($conn, $sql);
+//   $sql = "SELECT * FROM markers WHERE WEEK(created_at, 3) = WEEK(CURDATE(), 3) AND YEAR(created_at) = YEAR(CURDATE()) group by groupId order by created_at desc limit 1";
+
+
+/* This query uses a subquery to get the most recent created_at for each groupId, and then joins the
+markers table to the subquery on the groupId and created_at columns. It returns all columns from the
+markers table for the rows that match the most recent created_at for each groupId in the current
+week. Please note that you can use this query if created_at column is unique for each groupId. You
+can also use the order by created_at desc and limit 1 for each groupId. */
+
+$sql = "SELECT markers.* FROM markers JOIN (SELECT groupId, max(created_at) as max_created_at FROM markers WHERE created_at >= DATE_SUB(NOW(), INTERVAL 1 WEEK) AND created_at < NOW() GROUP BY groupId) as grouped_markers ON markers.groupId = grouped_markers.groupId AND markers.created_at = grouped_markers.max_created_at ORDER BY markers.created_at DESC LIMIT 1";
+$result = mysqli_query($conn, $sql);
 
   $geojson = array(
     'type'      => 'FeatureCollection',
