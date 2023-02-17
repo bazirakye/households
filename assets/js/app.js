ff61e085171bf64a05dab709ca62ac6bb8395aff
bazirakye tonny <bazirakyetonny15@gmail.com>
2023-02-17 16:41:40 +0300

    edited loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -581,12 +581,12 @@ var households = L.geoJson(null, {
       
           var distance = homeLatLng.distanceTo(currentLatLng);
     
-    
+          $('#loading').show();
+          
           if (distance <= 200) {
             L.marker(homeLatLng).addTo(map);
             L.marker([lat_current,lng_current]).addTo(map);
             navigator.geolocation.clearWatch(watchID);
-            $('#loading').show();
 
             $.ajax({
               url: 'markers.php',
@@ -610,6 +610,7 @@ var households = L.geoJson(null, {
             });
 
           }else{
+            $('#loading').hide();
             L.marker([lat_current,lng_current]).addTo(map);
             alert(["Sorry, You\'re not in the group location. You\'re ",distance, "meters away from the group"]);
             navigator.geolocation.clearWatch(watchID);
